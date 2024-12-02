0a9ff2147c2bc55674b62d720295770b97ff1381
4lessandroGobbo13 <alessandrogobbo19@gmail.com>
2024-12-02 09:33:22 +0100

    Commentato le funzioni presenti, e sistemato i grafici

--- dashboard.py
+++ dashboard.py
@@ -2,14 +2,29 @@ import streamlit as st
 import polars as pl
 import altair as alt
 
+'''
+Funzione che legge i dati del file csv del percorso: DATA/data.csv
 
+Return:
+    dataframe: dataframe dei dati
+'''
 @st.cache_data
 def read_data():
     data = pl.read_csv('DATA/data.csv')
     return data
 
+
 '''
-PRIMA RIGA DI GRAFICI
+Funzione che ritorna un diagramma a barre in cui l'asse delle X rappresenta gli anni, mentre
+l'asse delle Y rappresenta il numero di auto vendute. 
+
+-Crea un dataframe in cui sono presenti le colonne 'ANNO' e 'NUMERO VENDITE'
+-Creazione del grafico a barre con i dati ricavati in precedenza
+
+Param: 
+    dataframe: dataframe dei dati
+Return:
+    chart: grafico a barre creato con altair
 '''
 def year_pop_chart(data):
     data_chart = (
@@ -34,9 +49,16 @@ def year_pop_chart(data):
             x= 'Model Year:O'
         )
     )
-    #st.altair_chart(chart)
     return(chart)
 
+'''
+Testo che spiega il grafico che sarà presente alla sinistra della spiegazione
+
+Param:
+    int: numero di auto presenti nel database
+Return:
+    text: testo della spiegazione
+'''
 def text_year_pop_chart(number):
     text = f'''Dal grafico si nota come anno dopo anno
             la vendita di auto con motore elettrico o ibrido aumenta in modo 
@@ -49,10 +71,16 @@ def text_year_pop_chart(number):
             '''
     return(text)
 
-'''
-SECONDA RIGA DI GRAFICI
-'''
 
+'''
+Funzione che dal dataframe generato dal file csv, si vanno a selezionare i dati 
+del numero dei veicoli venduti per ogni produttore di auto presente nel dataset
+
+Param: 
+    dataframe: dataframe dei dati
+Return:
+    dataframe: dataframe che contiene PRODUTTORE e NUMERO DI VENDITE
+'''
 def make_pop_data(data):
     data = (
         data
@@ -64,6 +92,12 @@ def make_pop_data(data):
     )
     return(data)
 
+'''
+Testo che spiega il grafico che sarà presente alla destra della spiegazione
+
+Return:
+    text: testo della spiegazione
+'''
 def text_make_pop_data():
     text = f'''Nella tabella presente di lato si vede quante auto
             sono state vendute da ogni venditore presente nello stato di washington 
@@ -78,13 +112,31 @@ def text_make_pop_data():
             e cercheremo di capire i motivi. '''
     return(text)
 
-'''
-TERZA RIGA DI GRAFICI
-'''
 
+'''
+Funzione che serve a creare una lista in cui sono presenti tutti i produttori di veicoli
+elettrici e ibridi presenti nel dataframe
+
+Param: 
+    dataframe: dataframe dei dati 
+Return:
+    list: lista dei produttori
+'''
 def make_list(data):
     return data['Make'].unique().sort().to_list()
 
+'''
+Funzione che ha l'obiettivo di creare un grafico a linee 
+in cui in base ai produttore scelti dall'utente si vedranno il numero di auto 
+vendute da ogni produttore selezionato per ogni anno. 
+
+Param: 
+    dataframe: dataframe dei dati
+    list: lista dei produttori selezionati
+
+Return:
+    chart: grafico a linee
+'''
 def make_per_year(data, make):
     data = (
         data
@@ -115,8 +167,18 @@ def make_per_year(data, make):
 
     return(chart)
 
-def model_per_maker(data, make):
-    # Prepara i dati aggregati per marca e modello
+'''
+Funzione che va a generare grafici a torta per ogni produttore scelto dall'utente.
+Per ogni produttore saranno visibili il numero di modelli venduti. 
+Il numero massimo di grafici visibili sarà 3. 
+La creazione dei grafici viene a seguito di una manipolazione dei dati.
+
+Param:
+    dataframe: dataframe dei dati 
+    list: lista di produttore selezionati dall'utente
+'''
+def model_per_make(data, make):
+
     data_agg = (
         data
         .group_by('Make', 'Model')
@@ -126,23 +188,63 @@ def model_per_maker(data, make):
         .filter(pl.col('Make').is_in(make))
     )
 
-    # Crea il grafico
-    chart = alt.Chart(data_agg).mark_arc().encode(
-        theta=alt.Theta('Vendita_per_modello:Q'),  # Quantitativo per il calcolo delle dimensioni
-        color=alt.Color('Model:N'),  # Colore per ogni modello
-        tooltip=['Model', 'Vendita_per_modello']
-    ).properties(
-        width=200,
-        height=200
-    ).facet(
-        facet='Make:N',  # Crea una torta per ogni marca
-        columns=3  # Disponi 3 colonne
+    base_pie = (
+        alt.Chart(data_agg)
+        .mark_arc(
+            radius = 80,
+            radius2 = 120,
+            cornerRadius=20
+        )
+        .encode(
+            alt.Theta('Vendita_per_modello:Q'),
+            alt.Color('Model:N').legend(None)
+        )
     )
 
-    return chart
+    text_pie = (
+        alt.Chart(data_agg)
+        .mark_text(
+            radius = 150,
+            size = 15,
+            color='cyan'
+        )
+        .encode(
+            alt.Text('Model:N'),
+            alt.Theta('Vendita_per_modello:Q', stack=True),
+            alt.Order('Model:N')
+        )
+    )
 
+    text_total = (
+        alt.Chart(data_agg)
+        .mark_text(
+            radius = 0,
+            size=30,
+            color='cyan'
+        )
+        .encode(
+            alt.Text("Make:N")
+        )
+    )
 
+    chart = (
+        base_pie+text_pie+text_total
+    ).properties(
+        height = 30,
+        width = 30
+    ).facet(
+        'Make', columns=3
+    )
 
+    '''
+    Devo togliere dalla legenda i modelli che hanno poche vendite, altrimenti si sovrappongono, capire come fare
+    '''
+
+    return(chart)
+
+'''
+Funzione che va a generare la pagina di dashboard
+'''
 def dashboard_main():
 
     data = read_data()
@@ -175,8 +277,7 @@ def dashboard_main():
 
     #QUARTO CONTAINER
     c4 = st.container(border=False)
-
-    c4.altair_chart(model_per_maker(data, st.session_state.make_selection))
+    c4.altair_chart(model_per_make(data, st.session_state.make_selection), use_container_width=True)
 
     
 if __name__ == '__main__':
